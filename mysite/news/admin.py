0e519017fb8d6a37f1d98eeab11d77f8dc54041b
Roman Pavlov <pra_dev@mail.ru>
2022-03-02 15:27:04 +0500

    fixed admin register add admin models for both tables

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from news.models import News,Category
 # Register your models here.
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id','title','created_at','updated_at','is_published')
+    list_filter = ('category','is_published')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
 
-
-admin.register(News)
-admin.register(Category)
+admin.site.register(News,NewsAdmin)
+admin.site.register(Category,CategoryAdmin)
