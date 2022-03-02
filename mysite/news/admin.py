2bebb2a352237567340b7cb19fdab4de3f7ebc80
Roman Pavlov <pra_dev@mail.ru>
2022-03-02 15:22:24 +0500

    set new models News and Category register news app create super user

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from news.models import News,Category
 # Register your models here.
+
+
+
+
+admin.register(News)
+admin.register(Category)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+
 # Create your models here.
