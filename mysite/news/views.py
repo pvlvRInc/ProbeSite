56a0d371a847b950f8428d45a89637bb41b5a442
Roman Pavlov <pra_dev@mail.ru>
2022-03-12 13:37:19 +0500

    add card witn news items

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from news.models import News
 # Create your views here.
 #todo crate path with all existing news
 #todo make a template
 
 def main_path(request):
     print(request)
-    return render(request, 'news/main_path.html',{})
+    news = News.objects.all()
+    return render(request, 'news/main_path.html',{'news': news})
