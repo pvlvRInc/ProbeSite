767afc20481f55219453d8e9d019cb1eb870d14f
Roman Pavlov <pra_dev@mail.ru>
2022-03-24 19:44:47 +0500

    add get_absolute_url to categories

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+from django.shortcuts import render
+from django.urls import reverse
 
 
 class News(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
-    photo = models.ImageField(upload_to='media/%Y/%m/%d',verbose_name='Фото', blank=True)
+    photo = models.ImageField(upload_to='media/%Y/%m/%d', verbose_name='Фото', blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
-    #todo add image field
+
+    # todo add image field
 
     class Meta:
         verbose_name = 'Новость'
@@ -26,6 +29,9 @@ class Category(models.Model):
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
         ordering = ['id']
+
+    def get_absolute_url(self):
+        return reverse('category_news', kwargs={'category_id': self.pk})
 
     def __str__(self):
         return self.title
